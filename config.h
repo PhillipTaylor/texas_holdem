6a0d3b73f2e7e666d784b177e20a72a761f1cc3a
Phill <phill@eeepc.localdomain>
2008-12-16 19:38:26 +0000

    A configuration module shouldn't need set and delete functions.

--- config.h
+++ config.h
@@ -9,12 +9,3 @@ bool config_get_string(char *key, char **output);
 bool config_get_int(char *key, int **output);
 bool config_get_bool(char *key, bool **output);
 
-void config_set_string(char *key, char *value);
-void config_set_int(char *key, int *value);
-void config_set_bool(char *key, bool *value);
-
-void config_del_value(char *key);
-
-
-
-
