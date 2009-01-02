7b011e1bf7573cda05363fdd8ac9c2a9b4332b5c
Phill <phill@eeepc.localdomain>
2009-01-02 23:28:20 +0000

    This is a sad commit :-( The deletion of all the work I've spent the whole of christmas on :-( Hours of trying to write code that I've realised I can never actually achieve.
    I wanted a fork to wait for incoming network connections and a process for each table so that games can run independently. Unfortunetly the problem is that after a fork the processes are independant. You can't properly pass an open network socket between processes very easily. I have tried shget approach for shared memory, fifo pipes and sys v message queues but all of them have some drawbacks. In the end apparently the way to go is select() and poll() but I'm a little unsure of how I'll make this work. Poker is after all a turn based game rather than an Asyncronous chat room. I'll tackle those issues later.
    
    For now I'm just wiping out all the bad code recently added. Instead I'm concentrating on game logic. Specifically I want to ensure the "hand detection" algorithms are all working nicely since they were first written over a year ago. On top of this I want to start thinking about how I can write testing logic.

--- main.c
+++ main.c
@@ -20,50 +20,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/msg.h>
-#include <sys/wait.h>
 #include <errno.h>
 
-#include "linkedlist.h"
-#include "globals.h"
-#include "card.h"
-#include "logging.h"
-#include "stack.h"
-#include "servers.h"
-#include "player.h"
-
 #define VERSION "0.1"
-
-#define ERR -1
-
-/*
- * Main function runs, loads 5 named table objects. For each table
- * created a fork is created for that specific table object that
- * allows each table to run independently.
- *
- * Usernames details are also created and prepared. These are hardcoded
- * until I add database logic later.
- *
- * After the tables are prepared, the main function starts a server
- * listening thread that waits for UNIX sockets to connect to it. When
- * they arrive it spawns a handling thread that does a quick login check
- * and then asks the user which of the named tables they want to play on
- *
- * The socket information is then connected to the table, so the table can
- * then communicate with user. When the table is full, the table algorithm
- * is responcible for basically looping contineously, creating new "games"
- * until only one player is left. He is the winner. When he wins, the table
- * exits.
- *
- * the game loop does all the real work for the texas holdem game, hosting
- * rounds of bets, dealing cards etc.
- */
-
-void table_process(long tables_id);
-void main_game_loop(void);
 
 int main(int argc, char **argv)
 {
@@ -77,105 +36,16 @@ int main(int argc, char **argv)
 	int i;
 	int s;
 
-	printf("------------ PRT POKER -----------\nA texas holdem poker server\n~~~~Version: %s~~~~~~~\n", VERSION);
+	printf("------------ PRT POKER -----------\n");
+	printf("A texas holdem poker server\n");
+	printf("~~~~Version: %s~~~~~~~\n", VERSION);
 
 	config_load("poker.conf");
 	logging_init();
 
-	if (key = ftok("/mnt/mydocuments/git/texas_holdem/poker", 'a') == -1)
-	{
-		logging_critical("Call to ftok failed");
-		exit(1);
-	}
 
-	if ((msg_queue = msgget(key, 0644 | IPC_CREAT)) == -1)
-	{
-		logging_critical("call to msgget failed");
-		exit(1);
-	}
-		logging_info("System V Message Queue Id: %i", msg_queue);
-
-	//read int the list of tables from the configuration file
-
-	config_get_int("players_per_table", &player_count);
-	logging_info("players per table: %i", *player_count);
-
-	config_get_int("table_count", &table_count);
-	logging_info("number of tables: %i", *table_count);
-
-	table_names = malloc(sizeof(char*) * (*table_count));
-
-	logging_info("Table count: %d", *table_count);
-
-	for (i = 0; i < *table_count; i++)
-	{
-		sprintf(table_config_name, "table_%d", (i + 1));	
-		config_get_string(table_config_name, &table_name);
-
-		table_names[i] = table_name;
-
-		logging_info("Created table %d: %s", i, table_name);
-
-		if (fork() == 0)
-		{
-			table_process((long)i);
-			_exit(0);
-		}
-	}
-
-	main_process_id = getpid();
-
-	connection_process_id = fork();
-
-	if (connection_process_id == 0)
-	{
-		wait_for_players();
-		_exit(0);
-	}
-	else
-		waitpid(connection_process_id, &s, 0);
-
-	logging_info("Application ended");
+	logging_shutdown();
+	printf("Application ended");
 	return 0;
 }
 
-void table_process(long table_id)
-{
-	player **all_players;
-	player *p;
-	int players_added;
-
-	all_players = malloc(sizeof(player*) * (*player_count));
-
-	players_added = *player_count;
-	while (players_added > 0)
-	{
-		logging_info("table %i (%s) pid: %i recieving on mtype %i, waiting for players: %i",
-			table_id, table_names[table_id],
-			getpid(), (long)(table_id + MSG_QUEUE_OFFSET),
-			players_added);
-		
-		p = malloc(sizeof(player));
-
-		if (msgrcv(msg_queue, p, sizeof(player), (long)(table_id + MSG_QUEUE_OFFSET), 0) == -1)
-		{
-			logging_critical("recieving from message queue failed");
-			_exit(1);
-		}
-
-		logging_info("message recieved\n");	
-
-		all_players[players_added] = p;
-		players_added--;
-	}
-
-	logging_info("table %s ready to start!", table_names[table_id]);
-
-	player_broadcast(all_players, *player_count, "%s is ready to start!!!", table_names[table_id]);
-
-}
-
-void main_game_loop(void)
-{
-	logging_debug("main_game_loop");
-}
--- servers.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef SERVERS_H
-#define SERVERS_H
-
-int wait_for_players(void);
-void sigchld_handler(int s);
-void login_handshake(int client_fd);
-
-#endif
